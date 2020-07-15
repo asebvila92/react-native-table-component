c37d7e31853191211175f94f791ad23bf7b62207
Andres <asebvila@gmail.com>
2020-07-15 00:41:27 -0300

    add touchable to row

--- components/rows.js
+++ components/rows.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ViewPropTypes, Text, StyleSheet } from 'react-native';
+import { View, ViewPropTypes, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Cell } from './cell';
 import { sum } from '../utils';
 
@@ -10,17 +10,19 @@ export class Row extends Component {
   };
 
   render() {
-    const { data, style, widthArr, height, flexArr, textStyle, ...props } = this.props;
+    const { opacityPress, onRowPress, data, style, widthArr, height, flexArr, textStyle, ...props } = this.props;
     let width = widthArr ? sum(widthArr) : 0;
 
     return data ? (
-      <View style={[height && { height }, width && { width }, styles.row, style]}>
-        {data.map((item, i) => {
-          const flex = flexArr && flexArr[i];
-          const wth = widthArr && widthArr[i];
-          return <Cell key={i} data={item} width={wth} height={height} flex={flex} textStyle={textStyle} {...props} />;
-        })}
-      </View>
+      <TouchableOpacity activeOpacity={opacityPress} onPress={onRowPress}>
+        <View style={[height && { height }, width && { width }, styles.row, style]}>
+          {data.map((item, i) => {
+            const flex = flexArr && flexArr[i];
+            const wth = widthArr && widthArr[i];
+            return <Cell key={i} data={item} width={wth} height={height} flex={flex} textStyle={textStyle} {...props} />;
+          })}
+        </View>
+      </TouchableOpacity>
     ) : null;
   }
 }
